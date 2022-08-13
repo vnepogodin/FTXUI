669e3c579ebd8e253b9b0f4b2e9beb76d84c85b6
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2022-08-13 18:15:52 +0400

    👷 fix compilation with newer GCC

--- src/ftxui/dom/flexbox_helper.cpp
+++ src/ftxui/dom/flexbox_helper.cpp
@@ -12,6 +12,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wuseless-cast"
 #pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wdeprecated"
 #endif
 
 #include <range/v3/algorithm/max.hpp>
